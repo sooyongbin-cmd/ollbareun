c12d55b36617061b2f5116014dca0b6fdb57f36a
sooyongbin <sooyongbin@gmail.com>
2026-07-22 17:20:09 +0900

    docs: 불필요한 문서 삭제

---
+++ public/docs/documents/assets/index.md.DtVE3xzo.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as i,c as t,a2 as l}from"./chunks/framework.BUS5y9ZA.js";const f=JSON.parse('{"title":"올바름 HRM 프로젝트 문서","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),r={name:"index.md"};function n(o,a,s,h,d,m){return i(),t("div",null,[...a[0]||(a[0]=[l("",4)])])}const _=e(r,[["render",n]]);export{f as __pageData,_ as default};
